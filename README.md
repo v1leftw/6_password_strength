d0b43b92bbe66c3f3d2d7cbb120daf9391bd5d62
v1leftw <dmitry.g.fake@gmail.com>
2018-10-24 13:34:25 +0300

    [+] methods to calculate password strength [*] README.md edited

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # Password Strength Calculator
 
-[TODO. There will be project description]
+Script calculates score of the inputted password. User can't see inputted password.
+If password contains in [prohibition list](https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/darkweb2017-top100.txt) - returns 0.
+Repo of the lists - [here](https://github.com/danielmiessler/SecLists)
+
+# Quickstart
+Example of script launch on Linux, Python 3.5
+
+```
+$ python password_strength.py
+Input your password:
+Your score is 0.
+```
 
 # Project Goals
 
--- password_strength.py
+++ password_strength.py
@@ -1,35 +1,71 @@
 import re
 from string import punctuation
+from urllib import request
+import getpass
+
+'''
+List of top non-secure passwords:
+https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/darkweb2017-top100.txt
+Link was shortened below.
+'''
+PROHIBITION_LIST = "https://bit.ly/2AoqJzK"
 
 
-def get_password_strength(password):
-    pass
+def get_data_from_list():
+    try:
+        with request.urlopen(PROHIBITION_LIST, timeout=10000) as pw_list:
+            return pw_list.read().decode("utf-8")
+    except Exception:
+        return None
 
 
-def check_lower_and_upper_characters(pw):
-    if re.search(r"[a-zа-я]", pw) and re.search(r"[A-ZА-Я]", pw):
+def get_password_strength(user_pw):
+    if is_in_prohibition_list(user_pw):
+        return 0
+    current_score = set_scores_for_conditions(user_pw)
+    if len(user_pw) > 5:
+        return current_score * 2
+    return current_score
+
+
+def check_lower_and_upper_characters(user_pw):
+    if re.search(r"[a-zа-я]", user_pw) and re.search(r"[A-ZА-Я]", user_pw):
         return True
     else:
         return False
 
 
-def get_number_of_digits(pw):
-    return len(re.findall("[0-9]", pw))
+def get_number_of_digits(user_pw):
+    return len(re.findall("[0-9]", user_pw))
 
 
-def check_special_characters(pw):
-    if re.search(r"["+punctuation+"]", pw):
+def check_special_characters(user_pw):
+    if re.search(r'[{}]'.format(punctuation), user_pw):
         return True
     else:
         return False
 
 
-def check_if_contains_in_prohibition_lists(pw):
-    pass
+def is_in_prohibition_list(user_pw):
+    prohibition_list = get_data_from_list()
+    if prohibition_list is None:
+        return False
+    if user_pw in prohibition_list.split("\n"):
+        return True
+    return False
+
+
+def set_scores_for_conditions(user_pw):
+    score = 0
+    if check_lower_and_upper_characters(user_pw):
+        score = score + 2
+    if check_special_characters(user_pw):
+        score = score + 2
+    if get_number_of_digits(user_pw) > 2:
+        score = score + 1
+    return score
 
 
 if __name__ == "__main__":
-    password = input("Input your password: ")
-    print(check_lower_and_upper_characters(password))
-    print(get_number_of_digits(password))
-    print(check_special_characters(password))
+    password = getpass.getpass("Input your password: ")
+    print("Your score is {}.".format(get_password_strength(password)))
